837b13a3c53874c32e84521656eb0fc2fa6181b7
Lukasz <lu.kulakowski@gmail.com>
2021-06-07 03:01:09 -0400

    Added models and integrated those in admin panel

--- my_expenses/tracking/models.py
+++ my_expenses/tracking/models.py
@@ -1,15 +1,51 @@
 from django.db import models
 
 # Create your models here.
-class CostTypes(models.Model):
-    cost_name = models.CharField('Typy kosztow', max_length=200)
+#Cost frequency - ex monthly, yearly - this is currently a placeholder table, it will be used in a later phase
+class CostFreq(models.Model):
+    frequency = models.CharField('Czestotliwosc kosztow', max_length=200)
+    is_regular = models.BooleanField('Wydatek regularny')
+
+    class Meta:
+        verbose_name = "Czestotliwosc kosztu"
+        verbose_name_plural = "Czestotliwosc kosztow"
+
+    def __unicode__(self):
+        return self.frequency
+
+    def __str__(self):
+        return self.frequency
 
 
-class Categories(models.Model):
+#Categories 
+class Category(models.Model):
     category_name = models.CharField('Nazwa kategorii', max_length=200)
     category_icon = models.ImageField('Ikonka kategorii', upload_to='icons', blank=True)
-    cost_type = models.OneToOneField(CostTypes, verbose_name="Typy kosztow", on_delete=models.CASCADE)
+    cost_freq = models.OneToOneField(CostFreq, verbose_name="Czestotliwosc kosztow", on_delete=models.CASCADE)
 
-class Expenses(models.Model):
+    class Meta:
+        verbose_name = "Kategoria"
+        verbose_name_plural = "Kategorie"
+
+    def __unicode__(self):
+        return self.category_name
+
+    def __str__(self):
+        return self.category_name
+
+
+class Expense(models.Model):
     expense_date = models.DateField('Data wydatku')
     expense_amount = models.FloatField('Kwota wydatku ')
+    comment = models.CharField('Komentarz', max_length=1000)
+    category = models.OneToOneField(Category, verbose_name="Kategoria", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Wydatek"
+        verbose_name_plural = "Wydatki"
+
+    def __unicode__(self):
+        return f"{self.expense_amount} spent at {self.expense_date} on {self.category}"
+
+    def __str__(self):
+        return f"{self.expense_amount} spent at {self.expense_date} on {self.category}"
